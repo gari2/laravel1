3344a1f39f796521e6a8fc05229e8e875ce01235
gari2 <syamo.28.denkei@gmail.com>
2020-12-08 22:31:32 +0900

    5-29まで

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,50 +6,82 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
 use Validator;
+use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
 
    public function index(Request $request)
    {
-       $validator = Validator::make($request->query(), [
-           'id' => 'required',
-           'pass'=>'required',
-       ]);
-       if($validator->fails()) {
-           $msg = 'クエリーに問題があります。';
-       } else {
-           $msg = 'ID/PASSを受け付けました。フォームを入力下さい。';
-       }
-       return view('hello.index', ['msg'=> $msg,]);
+    //    $items = DB::table('people')->get();
+    //    return view('hello.index', ['items'=>$items]);
+    $items = DB::table('people')->orderBy('age', 'asc')->get();
+    return view('hello.index', ['items' => $items]);
    }
 
    public function post(Request $request)
    {
-    $rules = [
-        'name' => 'required',
-        'mail' => 'email',
-        'age' => 'numeric',
-    ];
-       $messages = [
-           'name.required'=>'名前は必ず入力してください。',
-           'mail.email'=>'メールアドレスが必要です。',
-           'age.numeric'=>'年齢を整数で入力してください',
-           'age.min'=>'年齢はゼロ歳以上で記入して下さい。',
-           'age.max'=>'年齢は200歳以下で記入して下さい。',
+       $items = DB::select('select * from people');
+       return view('hello.index', ['items'=>$items]);
+   }
+
+   public function add(Request $request)
+   {
+       return view('hello.add');
+   }
+
+   public function create(Request $request)
+   {
+       $param = [
+           'name' => $request->name,
+           'mail' => $request->mail,
+           'age'=> $request->age,
        ];
-       $validator = Validator::make($request->all(), $rules, $messages);
-       $validator->sometimes('age', 'min:0', function($input){
-           return !is_int($input->age);
-       });
-       $validator->sometimes('age', 'max:200', function($input){
-        return !is_int($input->age);
-       });
-       if($validator->fails()) {
-           return redirect('/hello')
-           ->withErrors($validator)
-           ->withInput();
-       }
-       return view('hello.index', ['msg'=>'正しく入力されました!']);
+       DB::table('people')->insert($param);
+       return redirect('/hello');
+   }
+   public function edit(Request $request)
+   {
+       $item = DB::table('people')
+       ->where('id', $request->id)->first();
+       return view('hello.edit', ['form'=>$item]);
+   }
+
+   public function update(Request $request)
+   {
+       $param = [
+           'name' => $request->name,
+           'mail' => $request->mail,
+           'age' => $request->age,
+       ];
+       DB::table('people')
+       ->where('id', $request->id)
+       ->update($param);
+       return redirect('/hello');
+   }
+
+   public function del(Request $request)
+   {
+       $item = DB::table('people')
+       ->where('id', $request->id)->first();
+       return view('hello.del', ['form' => $item]);
+   }
+
+   public function remove(Request $request)
+   {
+       DB::table('people')
+       ->where('id', $request->id)->delete();
+       return redirect('/hello');
+   }
+
+   public function show(Request $request)
+   {
+       $page = $request->page;
+       $items = DB::table('people')
+       ->offset($page * 3)
+       ->limit(3)
+       ->get();
+
+       return view('hello.show', ['items'=>$items]);
    }
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,7 +10,6 @@
 @section('content')
     <p>ここが本文のコンテンツです。</p>
     <p>必要なだけ記述できます。</p>
-<p>ViewComposer value<br>'view_message' = {{$view_message}}</p>
 <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
 <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
 
@@ -22,35 +21,16 @@
             これはメッセージの表示です。
         @endslot
     @endcomponent
-    <p>{{$msg}}</p>
-    @if (count($errors) > 0)
-        <p>入力に問題があります。再入力してください。</p>
-    @endif
-    <form action="/hello" method="POST">
-        <table>
-            @csrf
-            @error('name')
-                <tr><th>ERROR</th>
-                <td>{{$message}}</td></tr>
-            @enderror
-            <tr><th>name: </th><td><input type="text"
-            name="name" value="{{old('name')}}"></td></tr>
-            @error('mail')
-                <tr><th>ERROR</th>
-                <td>{{$message}}</td></tr>
-            @enderror
-            <tr><th>mail: </th><td><input type="text"
-            name="mail" value="{{old('mail')}}"></td></tr>
-            @error('age')
-                <tr><th>ERROR</th>
-                <td>{{$message}}</td></tr>
-            @enderror
-            <tr><th>age: </th><td><input type="text"
-            name="age" value="{{old('age')}}"></td></tr>
-            <tr><th></th><td><input type="submit"
-                value="send"></td></tr>
-        </table>
-    </form>
+    <table>
+        <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+        @foreach ($items as $item)
+            <tr>
+                <td>{{$item->name}}</td>
+                <td>{{$item->mail}}</td>
+                <td>{{$item->age}}</td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
 
 @section('footer')
