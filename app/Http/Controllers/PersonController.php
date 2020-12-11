123f5db6a16562acf65d8d214429a1d5497ab9f6
gari2 <syamo.28.denkei@gmail.com>
2020-12-11 22:13:27 +0900

    6-14まで

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -12,4 +12,25 @@ class PersonController extends Controller
         $items = Person::all();
         return view('person.index', ['items' => $items]);
     }
+
+    public function find(Request $request)
+    {
+        return view('person.find',['input' => '']);
+    }
+
+    public function search(Request $request)
+    {
+        // $item = Person::find($request->input);
+        // $param = ['input' => $request->input, 'item'=>$item];
+        // return view('person.find', $param);
+        // $item = Person::nameEqual($request->input)->first();
+        // $param = ['input' => $request->input, 'item'=>$item];
+        // return view('person.find', $param);
+        $min = $request->input + 1;
+        $max = $min + 10;
+        $item = Person::ageGreaterThan($min)->
+        ageLessThan($max)->first();
+        $param = ['input' => $request->input, 'item'=>$item];
+        return view('person.find', $param);
+    }
 }
