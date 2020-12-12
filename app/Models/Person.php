310e81b001861a3b61c21487656a366d4e9432ad
gari2 <syamo.28.denkei@gmail.com>
2020-12-12 14:32:39 +0900

    6-28

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -2,12 +2,21 @@
 
 namespace App\Models;
 
+use App\Scopes\ScopePerson;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
     use HasFactory;
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
     public function getData()
     {
         return $this->id . ': ' . $this->name . ' (' . $this->age . ')';
@@ -25,5 +34,12 @@ class Person extends Model
     public function scopeAgeLessThan($query, $n)
     {
         return $query->where('age', '<=', $n);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new ScopePerson);
     }
 }
